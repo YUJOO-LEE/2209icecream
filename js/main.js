a7dca824400db63e5cdcefe1630d43a22d845dd9
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-02 23:20:18 +0900

    카카오맵 API 적용

--- js/main.js
+++ js/main.js
@@ -122,3 +122,39 @@ contact.menus.forEach((el, i) => {
     contact.contents[i].classList.add('on');
   })
 })
+
+
+// 지도
+const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+const options = { //지도를 생성할 때 필요한 기본 옵션
+	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+	level: 3, //지도의 레벨(확대, 축소 정도)
+};
+
+const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+const marker = new kakao.maps.Marker({ 
+  // 지도 중심좌표에 마커를 생성합니다 
+  position: map.getCenter() 
+}); 
+// 지도에 마커를 표시합니다
+marker.setMap(map);
+
+const iwContent = `<div class="mapInfo">
+  <ul>
+    <li>YUJOO'S<br>ICE CREAM!</li>
+    <li>
+      <a href="https://map.kakao.com/link/map/YUJOO'S ICE CREAM,33.450701,126.570667" target="_blank">view detail</a>
+    </li>
+  </ul>
+  </div>`;
+// 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+const iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+
+// 인포윈도우를 생성합니다
+const infowindow = new kakao.maps.InfoWindow({
+    position : iwPosition, 
+    content : iwContent 
+});
+  
+// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+infowindow.open(map, marker); 
