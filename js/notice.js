e4428bf45f90e07789d544ad6a128445715d9814
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-07 19:03:55 +0900

    notice 페이지 개편

--- js/notice.js
+++ js/notice.js
@@ -1,4 +1,13 @@
 const board = document.querySelector('.board');
-const titles = board.querySelectorAll('li.title');
-const decses = board.querySelectorAll('li.decs');
+const lis = board.querySelectorAll('li');
 
+for (let i = 0; i < lis.length; i++) {
+  lis[i].addEventListener('click', (e)=>{
+    e.preventDefault();
+    for (let j = 0; j < lis.length; j++) {
+      if (lis[j] !== lis[i]) lis[j].classList.remove('on');
+    }
+
+    lis[i].classList.toggle('on');
+  })
+}
