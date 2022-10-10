e10d49998da5bc4651f5736e82b1e403ee83da08
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-10 18:57:34 +0900

    서브메뉴 추가

--- js/sub.js
+++ js/sub.js
@@ -3,7 +3,9 @@ const $header = document.querySelector('header');
 const header = {
   btnCall: $header.querySelector('.btnCall'),
   mobileNav: $header.querySelector('.mobileNav'),
-  logo: $header.querySelector('#logo')
+  logo: $header.querySelector('#logo'),
+  showSub: $header.querySelector('.showSub'),
+  subMenu: $header.querySelector('.subMenu')
 };
 
 
@@ -14,4 +16,12 @@ header.btnCall.addEventListener('click', (e)=>{ // 햄버거 버튼 클릭
   header.logo.classList.toggle('on');
   header.btnCall.classList.toggle('on');
   header.mobileNav.classList.toggle('on');
+})
+
+header.showSub.addEventListener('mouseenter', ()=>{
+  header.subMenu.classList.add('on');
+})
+
+$header.addEventListener('mouseleave', ()=>{
+  header.subMenu.classList.remove('on');
 })
